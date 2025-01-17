c777d500bb822c2aaa04c7c16f94d33251cd60d9
onief <o.nief@t-online.de>
2025-01-17 13:35:41 +0100

    Inital Commit

---
+++ src/main/java/uni/mainz/TrainingsTracker/TrainingsTrackerApplication.java
@@ -0,0 +1,13 @@
+package uni.mainz.TrainingsTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/mainz/TrainingsTracker/TrainingsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.mainz.TrainingsTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

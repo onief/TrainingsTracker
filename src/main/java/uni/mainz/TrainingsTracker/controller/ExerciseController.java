7cd5a1173e825fe6de0e2e4a38f96d01c5085d56
onief <o.nief@t-online.de>
2025-01-18 20:22:03 +0100

    Implement Get All Exercises

--- src/main/java/uni/mainz/TrainingsTracker/controller/ExerciseController.java
+++ src/main/java/uni/mainz/TrainingsTracker/controller/ExerciseController.java
@@ -1,4 +1,29 @@
 package uni.mainz.TrainingsTracker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uni.mainz.TrainingsTracker.model.Exercise;
+import uni.mainz.TrainingsTracker.repository.ExerciseRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exercise")
 public class ExerciseController {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(ExerciseController.class);
+    private final ExerciseRepository exerciseRepository;
+
+    public ExerciseController(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    @GetMapping("")
+    public List<Exercise> getAllExercises() {
+        return exerciseRepository.getAllExercises();
+    }
+
 }
--- src/main/java/uni/mainz/TrainingsTracker/repository/ExerciseRepository.java
+++ src/main/java/uni/mainz/TrainingsTracker/repository/ExerciseRepository.java
@@ -1,7 +1,27 @@
 package uni.mainz.TrainingsTracker.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import uni.mainz.TrainingsTracker.model.Exercise;
+
+import java.util.List;
 
 @Repository
 public class ExerciseRepository {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(ExerciseRepository.class);
+    private final JdbcClient jdbcClient;
+
+    public ExerciseRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Exercise> getAllExercises() {
+        return jdbcClient
+                .sql("select * from exercise")
+                .query(Exercise.class)
+                .list();
+    }
 }

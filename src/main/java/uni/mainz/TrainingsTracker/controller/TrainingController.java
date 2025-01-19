9fc78d98a9544447c1be5cfc741f07101afe9416
onief <o.nief@t-online.de>
2025-01-19 13:58:14 +0100

    Create Training Controller und Repository

---
+++ src/main/java/uni/mainz/TrainingsTracker/controller/TrainingController.java
@@ -0,0 +1,9 @@
+package uni.mainz.TrainingsTracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/training")
+public class TrainingController {
+}
---
+++ src/main/java/uni/mainz/TrainingsTracker/repository/TrainingRepository.java
@@ -0,0 +1,8 @@
+package uni.mainz.TrainingsTracker.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TrainingRepository {
+
+}

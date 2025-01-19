2761e725c955fe5eb457d5474e94ec470618e3d9
onief <o.nief@t-online.de>
2025-01-19 02:14:06 +0100

    Add Validation API

--- src/main/java/uni/mainz/TrainingsTracker/model/Set.java
+++ src/main/java/uni/mainz/TrainingsTracker/model/Set.java
@@ -1,10 +1,15 @@
 package uni.mainz.TrainingsTracker.model;
 
+import jakarta.validation.constraints.Positive;
+
 public record Set(
 
         int id,                     // unnecessary?
+        @Positive
         int order_number,
+        @Positive
         int weight,
+        @Positive
         int repetitions,
         int workout_exercise        // unnecessary?
 

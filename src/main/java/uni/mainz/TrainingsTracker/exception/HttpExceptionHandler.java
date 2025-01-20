072556bc10d5a8968a972d1b309537aab8727a33
onief <o.nief@t-online.de>
2025-01-20 18:32:55 +0100

    Implement Full Exception Handling to Meaningful Responses

--- src/main/java/uni/mainz/TrainingsTracker/exception/HttpExceptionHandler.java
+++ src/main/java/uni/mainz/TrainingsTracker/exception/HttpExceptionHandler.java
@@ -5,14 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.Instant;
 
 @ControllerAdvice
 public class HttpExceptionHandler {
 
+    // There must be a better way: both Implement ErrorResponse, but it doesn't include a message
     @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<HttpErrorResponse> handleRuntimeException(ResponseStatusException exception) {
+    public ResponseEntity<HttpErrorResponse> handleStatusException(ResponseStatusException exception) {
         HttpErrorResponse error = new HttpErrorResponse(
                 Instant.now(),
                 exception.getStatusCode().value(),
@@ -22,8 +24,19 @@ public class HttpExceptionHandler {
         return new ResponseEntity<>(error, exception.getStatusCode());
     }
 
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<HttpErrorResponse> handleStatusException(NoResourceFoundException exception) {
+        HttpErrorResponse error = new HttpErrorResponse(
+                Instant.now(),
+                exception.getStatusCode().value(),
+                exception.getStatusCode().toString(),
+                exception.getMessage()
+        );
+        return new ResponseEntity<>(error, exception.getStatusCode());
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<HttpErrorResponse> handleGeneralException(Exception ex) {
+    public ResponseEntity<HttpErrorResponse> handleGeneralException(Exception e) {
         HttpErrorResponse error = new HttpErrorResponse(
                 Instant.now(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
